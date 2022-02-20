caeb8c2d7aa5113ed26ec955df93e5573730a6ab
Alexander Polevshchikov <alex.p@nekidaem.ru>
2022-02-20 16:55:43 +0600

    Removed the limit

--- lib/data/chat/repository/firebase.dart
+++ lib/data/chat/repository/firebase.dart
@@ -19,7 +19,6 @@ class ChatRepositoryFirebase implements ChatRepository {
   Future<List<ChatMessageDto>> get messages async {
     final result = await _firebaseClient
         .collection(_messagesCollectionKey)
-        .limit(_messagesLimit)
         .orderBy('created')
         .get();
 
